97d8927f16f900c37300a0f5a433295521cee151
Narayan Sapkota <xanzong.naran29@gmail.com>
2025-04-19 15:47:39 -0400

    Added Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import MovieCard from "../components/MovieCard";
+
 function Home() {
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
@@ -8,9 +10,13 @@ function Home() {
 
   return (
     <div className="home">
-          <div className="movie-grid">
-              
+      <div className="movie-grid">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
       </div>
     </div>
   );
 }
+
+export default Home;
