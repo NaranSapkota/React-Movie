332e877848abacf29fa0f637bcfb06870fc1cefe
Narayan Sapkota <xanzong.naran29@gmail.com>
2025-04-19 16:06:55 -0400

    Alert

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
     { id: 2, title: "Terminator", release_date: "1998" },
@@ -8,11 +11,9 @@ function Home() {
     { id: 4, title: "Spiderman 4", release_date: "2023" },
   ];
 
-    const handleSearch = () => {
-      
-
-    };
-    
+  const handleSearch = () => {
+    alert("searchQuery");
+  };
 
   return (
     <div className="home">
@@ -21,7 +22,12 @@ function Home() {
           type="text"
           placeholder="Search for a movie..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
+        <button type="submit" className="search-button">
+          Search
+        </button>
       </form>
       <div className="movie-grid">
         {movies.map((movie) => (
